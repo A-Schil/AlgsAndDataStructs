0da3fed44f97c45c4018adc4d65563f8bfd95ced
Alex Schillinger <alexcschillinger@gmail.com>
2017-10-25 22:57:21 -0500

    mergesort doesn't need sentinels

--- src/main/groovy/com/github/aschil/sorting/MergeSort.groovy
+++ src/main/groovy/com/github/aschil/sorting/MergeSort.groovy
@@ -2,48 +2,45 @@ package com.github.aschil.sorting
 
 class MergeSort {
     static void sort(List<Integer> list) {
-        int[] array = new int[list.size()]
-        list.eachWithIndex { it, idx -> array[idx] = it }
-        mergeSort(array, 0, list.size() - 1)
-        (0..(list.size() - 1)).each {
-            list[it] = array[it]
-        }
+        mergeSort(list, 0, list.size() - 1)
     }
 
-    static void mergeSort(int[] array, int startingIndex, int endingIndex) {
+    static void mergeSort(List<Integer> list, int startingIndex, int endingIndex) {
         if (startingIndex < endingIndex) {
             int splitIndex = Math.floor((startingIndex + endingIndex) / 2)
-            mergeSort(array, startingIndex, splitIndex)
-            mergeSort(array, splitIndex + 1, endingIndex)
-            merge(array, startingIndex, splitIndex, endingIndex)
+            mergeSort(list, startingIndex, splitIndex)
+            mergeSort(list, splitIndex + 1, endingIndex)
+            merge(list, startingIndex, splitIndex, endingIndex)
         }
     }
 
-    static void merge(int[] array, int startingIndex, int splitIndex, int endingIndex) {
-        int lengthOfLeftSubArray = splitIndex - startingIndex + 1 // 3
-        int lengthOfRightSubArray = endingIndex - splitIndex  // 2
-        // TODO optimize to not copy
-        int[] leftSubArray = new int[lengthOfLeftSubArray + 1]
-        (0..(lengthOfLeftSubArray - 1)).each {
-            leftSubArray[it] = array[startingIndex + it]
+    static void merge(List<Integer> list, int startingIndex, int splitIndex, int endingIndex) {
+        int lengthOfLeftSubList = splitIndex - startingIndex + 1 // 3
+        int lengthOfRightSubList = endingIndex - splitIndex  // 2
+
+        int[] leftSubList = new int[lengthOfLeftSubList + 1]
+        (0..(lengthOfLeftSubList - 1)).each {
+            leftSubList[it] = list[startingIndex + it]
         }
-        int[] rightSubArray = new int[lengthOfRightSubArray + 1]
-        (0..(lengthOfRightSubArray - 1)).each {
-            rightSubArray[it] = array[splitIndex + 1 + it]
+        int[] rightSubList = new int[lengthOfRightSubList + 1]
+        (0..(lengthOfRightSubList - 1)).each {
+            rightSubList[it] = list[splitIndex + 1 + it]
         }
-        leftSubArray[lengthOfLeftSubArray] = Integer.MAX_VALUE
-        rightSubArray[lengthOfRightSubArray] = Integer.MAX_VALUE
+
         int leftIndex = 0
         int rightIndex = 0
         (startingIndex..endingIndex).each {
-            if(leftSubArray[leftIndex] <= rightSubArray[rightIndex]) {
-                array[it] = leftSubArray[leftIndex]
+            if (leftIndex > lengthOfLeftSubList - 1) {
+                list[it] = rightSubList[rightIndex]
+                rightIndex++
+            } else if (rightIndex > lengthOfRightSubList - 1 ||
+                    leftSubList[leftIndex] <= rightSubList[rightIndex]) {
+                list[it] = leftSubList[leftIndex]
                 leftIndex++
             } else {
-                array[it] = rightSubArray[rightIndex]
+                list[it] = rightSubList[rightIndex]
                 rightIndex++
             }
         }
-
     }
 }
